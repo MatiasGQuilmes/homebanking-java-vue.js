04f5c4e78b2caa539e490ac4ac95199d51ac8716
Matias Gonzalez <matyyassgonzalez89@gmail.com>
2022-11-09 13:53:09 -0300

    implement loans

---
+++ src/main/java/com/mindhub/homebanking/service/LoanService.java
@@ -0,0 +1,2 @@
+package com.mindhub.homebanking.service;public interface LoanService {
+}
---
+++ src/main/java/com/mindhub/homebanking/service/imprement/AccountServiceImpletement.java
@@ -0,0 +1,2 @@
+package com.mindhub.homebanking.service.imprement;public class AccountServiceImpletement {
+}
